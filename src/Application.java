d446dc8222a00207d859680f44ed07c51b0caa47
sydneywoodruff@knights.ucf.edu <Ling-lingwalle1230>
2019-11-02 18:44:12 -0400

    Woodruff_PA4

--- src/Application.java
+++ src/Application.java
@@ -3,14 +3,14 @@ import java.io.IOException;
 public class Application{
 
     public static void main(String[] args) throws IOException{
-        DuplicateRemover remove = new DuplicateRemover();
-        remove.duplicateRemover("problem1.txt");
-        remove.write("unique_words.txt");
+        DuplicateRemover duplicateRemover = new DuplicateRemover();
+        duplicateRemover.remove("problem1.txt");
+        duplicateRemover.write("unique_words.txt");
         System.out.println("DuplicateRemover complete");
 
-        DuplicateCounter count = new DuplicateCounter();
-        count.duplicateCounter("problem2.txt");
-        count.write("unique_word_counts.txt");
+        DuplicateCounter duplicateCounter = new DuplicateCounter();
+        duplicateCounter.count("problem2.txt");
+        duplicateCounter.write("unique_word_counts.txt");
         System.out.println("DuplicateCounter complete");
     }
 
